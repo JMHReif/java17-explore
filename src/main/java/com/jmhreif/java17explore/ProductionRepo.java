db64b142b8b54fa0418269a472aed07c474c1b65
Jennifer Reif <jennifer.reif@neo4j.com>
2022-08-08 15:40:11 -0500

    Tweak repo queries, update readme with preso link

--- src/main/java/com/jmhreif/java17explore/ProductionRepo.java
+++ src/main/java/com/jmhreif/java17explore/ProductionRepo.java
@@ -4,9 +4,9 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 
 public interface ProductionRepo extends Neo4jRepository<Production, String> {
-    @Query("MATCH (m:Movie) RETURN *;")
+    @Query("MATCH (m:Movie) RETURN m;")
     Iterable<Production> findMovies();
 
-    @Query("MATCH (s:Show) RETURN *;")
+    @Query("MATCH (s:Show) RETURN s;")
     Iterable<Production> findShows();
 }
